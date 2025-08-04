47127be462223dffe3d23ae6d20f362f10b1f48e
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-04 18:01:47 +0700

    add processor config

--- models/internvl/processor.py
+++ models/internvl/processor.py
@@ -1,18 +1,35 @@
-
 from typing import Dict, List
 import jax.numpy as jnp
-
+import dataclasses
 from transformers import AutoProcessor
 from models.internvl.types import CONVERSATION
 
+@dataclasses.dataclass(slots=True, frozen=True)
+class InternVLProcessorConfig(object):
+    model_id: str = "OpenGVLab/InternVL3-1B-hf"
+    start_image_token: str = "<img>"
+    start_image_token_id: int = 151665
+    end_image_token: str = "</img>"
+    end_image_token_id: int = 151666
+    context_image_token:str = "<IMG_CONTEXT>"
+    context_image_token_id: int = 151667
 
 class InternVLProcessor(object):
-    def __init__(self, model_id:str = "OpenGVLab/InternVL3-1B-hf"):
+    def __init__(self, config:InternVLProcessorConfig = InternVLProcessorConfig()):
         r"""
         Args:
             model_id (str): model id on hf or local dir
         """
-        self._processor = AutoProcessor.from_pretrained(model_id)
+        self.config = config
+        self._processor = AutoProcessor.from_pretrained(config.model_id)
+
+        # check id miss-match
+        assert self._processor.tokenizer.start_image_token == self.config.start_image_token
+        assert self._processor.tokenizer.start_image_token_id == self.config.start_image_token_id
+        assert self._processor.tokenizer.end_image_token == self.config.end_image_token
+        assert self._processor.tokenizer.end_image_token_id == self.config.end_image_token_id
+        assert self._processor.tokenizer.context_image_token == self.config.context_image_token
+        assert self._processor.tokenizer.context_image_token_id == self.config.context_image_token_id
     
     def __call__(self, message: CONVERSATION):
         inputs = self._processor.apply_chat_template(
@@ -26,25 +43,25 @@ class InternVLProcessor(object):
 
     @property
     def start_image_token(self)->str:
-        return self._processor.tokenizer.start_image_token
+        return self.config.start_image_token
 
     @property
     def start_image_token_id(self)->int:
-        return self._processor.tokenizer.start_image_token_id
+        return self.staconfig.rt_image_token_id
     
     @property
     def end_image_token(self)->str:
-        return self._processor.tokenizer.end_image_token
+        return self.config.end_image_token
     
     @property
     def end_image_token_id(self)->int:
-        return self._processor.tokenizer.end_image_token_id
+        return self.config.end_image_token_id
 
     @property
     def context_image_token(self)->str:
-        return self._processor.tokenizer.context_image_token
+        return self.config.context_image_token
     
     @property
     def context_image_token_id(self)->int:
-        return self._processor.tokenizer.context_image_token_id
+        return self.config.context_image_token_id
     
