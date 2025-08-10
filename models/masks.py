055bb297316e66fa7037bcdc81d37e9116023219
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-10 20:22:55 +0700

    add mask with document, remove create position ids from input mask

--- models/masks.py
+++ models/masks.py
@@ -1,4 +1,9 @@
-from models.types import INPUT_IDS_TYPE, INPUT_MASK_TYPE, ATTENTION_MASK_TYPE, POSITION_IDS_TYPE
+from models.types import (
+    INPUT_IDS_TYPE, 
+    INPUT_MASK_TYPE, 
+    ATTENTION_MASK_TYPE, 
+    POSITION_IDS_TYPE
+)
 from models.utils import typechecked
 from models import MODE
 import jax.numpy as jnp
@@ -6,14 +11,11 @@ import jaxtyping
 
 @typechecked(mode= MODE)
 def make_causal_attn_mask(
-        input_ids: INPUT_IDS_TYPE,
         input_mask: INPUT_MASK_TYPE,
-        start_img_token_id:int,
-        context_img_token_id:int,
-        end_img_token_id:int
     )->ATTENTION_MASK_TYPE:
     """
-    Makes a causal attention mask.
+    Makes standar causal attention mask
+
     Args:
         input_mask (`INPUT_MASK_TYPE`): Input mask for the input. True 
         for non-padded tokens only, else False.
@@ -24,6 +26,129 @@ def make_causal_attn_mask(
     causal_mask = jnp.tril(jnp.ones((seq_len, seq_len), dtype=jnp.bool))
     attn_mask = input_mask[..., None, :]
     attn_mask *= causal_mask[None, ...]
+    return attn_mask
+
+
+@typechecked(mode= MODE)
+def make_causal_attn_mask_for_documents(
+        input_mask: INPUT_MASK_TYPE,
+        position_ids: POSITION_IDS_TYPE
+    )->ATTENTION_MASK_TYPE:
+    """
+    Makes a causal attention mask between documents, usefull in \
+    pre-training where tokens in each document only attend together
+
+    Args:
+        - input_mask (`INPUT_MASK_TYPE`): Input mask for the input. True 
+        for non-padded tokens only, else False.
+        - position_ids (`POSITION_IDS_TYPE`): position ids indice documents \
+        with start from 0 to some ID value, it may contains 0 or pad token id \ 
+        e.g [[0,1,2,3,4,0,1,2,0,0],[0,1,2,0,1,2,3,0,0,0]]
+    Returns:
+        Attention mask (`ATTENTION_MASK_TYPE`)
+    
+    Example:
+        - args:
+            - input_mask = jnp.array(
+                [[1,1,1,1,1,1,1,1,0,0],
+                [1,1,1,1,1,1,1,0,0,0]], 
+                dtype = jnp.bool_
+                )
+
+            - position_ids = jnp.array(
+                [[0,1,2,3,4,0,1,2,0,0],
+                [0,1,2,0,1,2,3,0,0,0]]
+                , dtype = jnp.int8
+            )
+        - returns:
+            [[[1 0 0 0 0 0 0 0 0 0]  # element 1 in batch
+            [1 1 0 0 0 0 0 0 0 0]
+            [1 1 1 0 0 0 0 0 0 0]
+            [1 1 1 1 0 0 0 0 0 0]
+            [1 1 1 1 1 0 0 0 0 0]
+            [0 0 0 0 0 1 0 0 0 0]
+            [0 0 0 0 0 1 1 0 0 0]
+            [0 0 0 0 0 1 1 1 0 0]
+            [0 0 0 0 0 0 0 0 0 0]    # 2 last pad tokens are ignored
+            [0 0 0 0 0 0 0 0 0 0]]
+
+            [[1 0 0 0 0 0 0 0 0 0]   # element 2 in batch
+            [1 1 0 0 0 0 0 0 0 0]
+            [1 1 1 0 0 0 0 0 0 0]
+            [0 0 0 1 0 0 0 0 0 0]
+            [0 0 0 1 1 0 0 0 0 0]
+            [0 0 0 1 1 1 0 0 0 0]
+            [0 0 0 1 1 1 1 0 0 0]
+            [0 0 0 0 0 0 0 0 0 0]   # 3 last pad tokens are ignored
+            [0 0 0 0 0 0 0 0 0 0]
+            [0 0 0 0 0 0 0 0 0 0]]]
+    """
+    attn_mask = make_causal_attn_mask(input_mask)
+
+    # count for pad token if pad token id is zero
+    # will be removed later by `attn_mask`
+    mask = (position_ids == 0)
+
+    # use cumsum, tokens in same document will have
+    # same value
+    # e.g: 
+    # [1 1 1 1 1 2 2 2 3 4]
+    # [1 1 1 2 2 2 2 3 4 5]] 
+    mask = jnp.cumsum(mask, axis= -1)
+
+    # True where it have same value
+    # e.g:
+    # [[[1 1 1 1 1 0 0 0 0 0]
+    # [1 1 1 1 1 0 0 0 0 0]
+    # [1 1 1 1 1 0 0 0 0 0]
+    # [1 1 1 1 1 0 0 0 0 0]
+    # [1 1 1 1 1 0 0 0 0 0]
+    # [0 0 0 0 0 1 1 1 0 0]
+    # [0 0 0 0 0 1 1 1 0 0]
+    # [0 0 0 0 0 1 1 1 0 0]
+    # [0 0 0 0 0 0 0 0 1 0]
+    # [0 0 0 0 0 0 0 0 0 1]]
+
+    # [[1 1 1 0 0 0 0 0 0 0]
+    # [1 1 1 0 0 0 0 0 0 0]
+    # [1 1 1 0 0 0 0 0 0 0]
+    # [0 0 0 1 1 1 1 0 0 0]
+    # [0 0 0 1 1 1 1 0 0 0]
+    # [0 0 0 1 1 1 1 0 0 0]
+    # [0 0 0 1 1 1 1 0 0 0]
+    # [0 0 0 0 0 0 0 1 0 0]
+    # [0 0 0 0 0 0 0 0 1 0]
+    # [0 0 0 0 0 0 0 0 0 1]]]
+
+    docmask = mask[:, None, :] == mask[..., None]
+    return attn_mask & docmask
+
+@typechecked(mode= MODE)
+def make_causal_attn_mask_for_vision(
+        input_ids: INPUT_IDS_TYPE,
+        input_mask: INPUT_MASK_TYPE,
+        start_img_token_id:int,
+        context_img_token_id:int,
+        end_img_token_id:int
+    )->ATTENTION_MASK_TYPE:
+    """
+    Makes a causal attention mask for vision-language model where \
+    text tokens are attented in causal way and vision tokens can \
+    attented in bidirectinal way.
+
+    Args:
+        - input_ids (`INPUT_IDS_TYPE`): input token of texts
+        - input_mask (`INPUT_MASK_TYPE`): Input mask for the input. True 
+        for non-padded tokens only, else False.
+        - start_img_token_id (int): taken from tokenizer
+        - context_img_token_id (int): taken from tokenizer
+        - end_img_token_id (int): taken from tokenizer
+    Returns:
+        Attention mask (`ATTENTION_MASK_TYPE`)
+
+    NOTE: this implement rely on sepecial tokens in `input_ids`
+    """
+    attn_mask = make_causal_attn_mask(input_mask)
 
     bidirectional_mask = (input_ids == start_img_token_id) | \
         (input_ids == context_img_token_id) | \
@@ -56,19 +181,3 @@ def _make_block_mask_indices(
     boundary = padded_mask[..., 1:] > padded_mask[..., :-1]
     numbered_boundary = jnp.cumsum(boundary, axis=-1)
     return bidirectional_mask * numbered_boundary
-
-@typechecked(mode= MODE)
-def get_positions_from_mask(input_mask: INPUT_MASK_TYPE) -> POSITION_IDS_TYPE:
-    """Computes the `positions` from the `input_mask`.
-
-    Args:
-        input_mask: The tokens `input_mask`, True for non-padded tokens only.
-
-    Returns:
-        The indices to use for RoPE and absolute position encodings for the given
-        input mask.
-    """
-    positions = jnp.cumsum(input_mask, axis=-1)
-    # Subtract one for all positions from the first valid one as they are
-    # 0-indexed
-    return positions - (positions >= 1)
