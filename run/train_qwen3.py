9a37d808b8a68b14ccfd1aea542aebf37638770e
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-11 17:19:52 +0700

    add to sys path

--- run/train_qwen3.py
+++ run/train_qwen3.py
@@ -1,3 +1,11 @@
+import os 
+import sys
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+
 from models.qwen3.params import create_model_from_safe_tensors
 from models.qwen3.configs import Qwen3Config
 
