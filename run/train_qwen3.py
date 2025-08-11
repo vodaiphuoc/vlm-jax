df8fc99cfafd9c2b6bc1682f7fce12001e6b4aee
vodaiphuoc <phuocvodn98@gmail.com>
2025-08-11 17:14:51 +0700

    testing

--- run/train_qwen3.py
+++ run/train_qwen3.py
@@ -1,6 +1,14 @@
 from models.qwen3.params import create_model_from_safe_tensors
 from models.qwen3.configs import Qwen3Config
 
+import jax
+# from jax.sharding import Mesh, PartitionSpec, NamedSharding
+
+MESH = [(1, 8), ("fsdp", "tp")]
 config = Qwen3Config.qwen3_1_7_b()
 
-model = create_model_from_safe_tensors(config)
+mesh = jax.make_mesh(*MESH)
+
+model = create_model_from_safe_tensors(config = config, mesh = mesh)
+
+print(model)
